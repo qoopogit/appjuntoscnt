58ba996194bccee2f44864988e2682366dce03fb
Dimater <admin@dimater.com>
2022-06-19 12:32:29 -0500

    intro base

--- src/app/intro/intro.module.ts
+++ src/app/intro/intro.module.ts
@@ -5,16 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { IntroPageRoutingModule } from './intro-routing.module';
-
+import { SwiperModule } from 'swiper/angular';
+import { Component } from '@angular/core';
 import { IntroPage } from './intro.page';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
 
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    IntroPageRoutingModule
+    IntroPageRoutingModule,
+    SwiperModule
   ],
   declarations: [IntroPage]
 })
+
+
 export class IntroPageModule {}
