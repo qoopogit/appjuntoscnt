be25367b319322f08813013cac989a364acbc33b
Alberto Garcia <alberto.garcia@qoopo.net>
2022-07-01 11:38:08 -0500

    [!] Fix merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,83 +6,108 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'principal',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalPageModule)
+    loadChildren: () =>
+      import('./principal/principal.module').then((m) => m.PrincipalPageModule),
   },
   {
     path: 'intro',
-    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
+    loadChildren: () =>
+      import('./intro/intro.module').then((m) => m.IntroPageModule),
   },
   {
     path: 'principal',
-    loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalPageModule)
+    loadChildren: () =>
+      import('./principal/principal.module').then((m) => m.PrincipalPageModule),
   },
   {
     path: 'acerca',
-    loadChildren: () => import('./acerca/acerca.module').then( m => m.AcercaPageModule)
+    loadChildren: () =>
+      import('./acerca/acerca.module').then((m) => m.AcercaPageModule),
   },
   {
     path: 'hacer',
-    loadChildren: () => import('./hacer/hacer.module').then( m => m.HacerPageModule)
-  },  {
-<<<<<<< HEAD
-    path: 'acerca',
-    loadChildren: () => import('./acerca/acerca.module').then( m => m.AcercaPageModule)
+    loadChildren: () =>
+      import('./hacer/hacer.module').then((m) => m.HacerPageModule),
   },
+
   {
     path: 'test',
-    loadChildren: () => import('./test/test.module').then( m => m.TestPageModule)
+    loadChildren: () =>
+      import('./test/test.module').then((m) => m.TestPageModule),
   },
   {
     path: 'test-cyber-acoso',
-    loadChildren: () => import('./test-cyber-acoso/test-cyber-acoso.module').then( m => m.TestCyberAcosoPageModule)
+    loadChildren: () =>
+      import('./test-cyber-acoso/test-cyber-acoso.module').then(
+        (m) => m.TestCyberAcosoPageModule
+      ),
   },
   {
     path: 'test-soy-violento',
-    loadChildren: () => import('./test-soy-violento/test-soy-violento.module').then( m => m.TestSoyViolentoPageModule)
+    loadChildren: () =>
+      import('./test-soy-violento/test-soy-violento.module').then(
+        (m) => m.TestSoyViolentoPageModule
+      ),
   },
   {
     path: 'test-sufro-mobbing',
-    loadChildren: () => import('./test-sufro-mobbing/test-sufro-mobbing.module').then( m => m.TestSufroMobbingPageModule)
+    loadChildren: () =>
+      import('./test-sufro-mobbing/test-sufro-mobbing.module').then(
+        (m) => m.TestSufroMobbingPageModule
+      ),
   },
   {
     path: 'test-sufro-violencia',
-    loadChildren: () => import('./test-sufro-violencia/test-sufro-violencia.module').then( m => m.TestSufroViolenciaPageModule)
-=======
+    loadChildren: () =>
+      import('./test-sufro-violencia/test-sufro-violencia.module').then(
+        (m) => m.TestSufroViolenciaPageModule
+      ),
+  },
+  {
     path: 'mujeres',
-    loadChildren: () => import('./sugerencias/mujeres/mujeres.module').then( m => m.MujeresPageModule)
+    loadChildren: () =>
+      import('./sugerencias/mujeres/mujeres.module').then(
+        (m) => m.MujeresPageModule
+      ),
   },
   {
     path: 'hombres',
-    loadChildren: () => import('./sugerencias/hombres/hombres.module').then( m => m.HombresPageModule)
+    loadChildren: () =>
+      import('./sugerencias/hombres/hombres.module').then(
+        (m) => m.HombresPageModule
+      ),
   },
   {
     path: 'amigos',
-    loadChildren: () => import('./sugerencias/amigos/amigos.module').then( m => m.AmigosPageModule)
+    loadChildren: () =>
+      import('./sugerencias/amigos/amigos.module').then(
+        (m) => m.AmigosPageModule
+      ),
   },
   {
     path: 'denuncia',
-    loadChildren: () => import('./sugerencias/denuncia/denuncia.module').then( m => m.DenunciaPageModule)
+    loadChildren: () =>
+      import('./sugerencias/denuncia/denuncia.module').then(
+        (m) => m.DenunciaPageModule
+      ),
   },
   {
     path: 'apoyo',
-    loadChildren: () => import('./apoyo/apoyo.module').then( m => m.ApoyoPageModule)
->>>>>>> 20e8a374d23ec4313a57a33b4d81f118374af0c8
-  }
-
-
-
+    loadChildren: () =>
+      import('./apoyo/apoyo.module').then((m) => m.ApoyoPageModule),
+  },
 ];
 
 @NgModule({
   imports: [
-    BrowserModule, HttpClientModule,
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 //import { WaitUtil } from './api/WaitUtil';
@@ -9,10 +8,9 @@ import { Service } from './api/Service';
 import { Api } from './api/Api';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
-<<<<<<< HEAD
+
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
@@ -25,11 +23,7 @@ import { HttpClientModule } from '@angular/common/http';
     Api,
     //WaitUtil,
   ],
-=======
-  imports: [BrowserModule,
-    HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
->>>>>>> 20e8a374d23ec4313a57a33b4d81f118374af0c8
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
