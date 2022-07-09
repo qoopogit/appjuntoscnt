b2d56a2aa553c0b03db66c2609dbb4894376b8b1
Dimater <admin@dimater.com>
2022-07-09 00:40:06 -0500

    testimonios

--- src/app/sugerencias/amigos/amigos.page.ts
+++ src/app/sugerencias/amigos/amigos.page.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-amigos',
   templateUrl: './amigos.page.html',
   styleUrls: ['./amigos.page.scss'],
 })
 export class AmigosPage implements OnInit {
+  titulo ='';
+  imagenprincial ='';
+  body ='';
+  texto1='';
+  constructor() {
 
-  constructor() { }
+    axios.get('https://uploads.bayoli.com/cmsjuntas.php?pagina=amigos')
+    .then(res => {
+
+      this.imagenprincial= res.data.imagen_princial;
+      this.titulo= res.data.titulo;
+      this.body= res.data.body;
+      this.texto1= res.data.texto1;
+      console.log(res.data, );
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+   }
 
   ngOnInit() {
   }
