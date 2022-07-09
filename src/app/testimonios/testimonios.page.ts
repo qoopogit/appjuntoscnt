e7c8299eb3b14799d78c98aa14f32d2a6c08a0e6
Dimater <admin@dimater.com>
2022-07-09 03:02:30 -0500

    socialsharing
    npm i cordova-plugin-x-socialsharing

--- src/app/testimonios/testimonios.page.ts
+++ src/app/testimonios/testimonios.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 import axios from 'axios';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+import { IonicSlides } from '@ionic/angular';
+
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 @Component({
   selector: 'app-testimonios',
   templateUrl: './testimonios.page.html',
@@ -9,16 +12,18 @@ import axios from 'axios';
 export class TestimoniosPage implements OnInit {
   imagenprincial ='';
   titulo='';
-  body ='';
+  body = [];
   constructor() {
 
     axios.get('https://uploads.bayoli.com/cmsjuntas.php?pagina=testimonios')
     .then(res => {
-
+      const i =0;
       this.imagenprincial= res.data.imagen_princial;
       this.titulo= res.data.titulo;
-      this.body= res.data.body;
       console.log(res.data, );
+
+      this.body= res.data.body;
+
     })
     .catch(err => {
       console.log(err);
