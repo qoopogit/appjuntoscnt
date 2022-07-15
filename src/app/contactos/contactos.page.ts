ce0e343595d71006dfa4edacaac330c02cb9b9c4
Dimater <admin@dimater.com>
2022-07-15 12:27:35 -0500

    mis contactos

--- src/app/contactos/contactos.page.ts
+++ src/app/contactos/contactos.page.ts
@@ -3,6 +3,7 @@ import { SMS } from '@awesome-cordova-plugins/sms/ngx';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { DbService } from './../api/db.service';
 import { Router } from '@angular/router';
+import axios from 'axios';
 
 import {
   ToastController,
@@ -19,13 +20,31 @@ import { Service } from '../api/Service';
 export class ContactosPage implements OnInit {
   mainForm: FormGroup;
   Data: any[] = [];
+  imagenprincial ='';
+  titulo='';
+  body = '';
 
   constructor(
     private db: DbService,
     public formBuilder: FormBuilder,
     private toast: ToastController,
     private router: Router
-  ) {}
+  ) {
+
+    axios.get('https://uploads.bayoli.com/cmsjuntas.php?pagina=contactos')
+    .then(res => {
+
+      this.imagenprincial= res.data.imagen_princial;
+      this.titulo= res.data.titulo;
+      this.body= res.data.body;
+      console.log(res.data.body, );
+
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  }
 
   /**
    * Elimina un registro
