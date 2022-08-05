b4f3f2c69ea90cfe3c1b5c802a62a4d99a942b2e
Alberto Garcia <alberto.garcia@qoopo.net>
2022-08-05 16:30:02 -0500

    [*]  Se agrega back button

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -7,7 +7,7 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-//import ch.byrds.capacitor.contacts.Contacts;
+import ch.byrds.capacitor.contacts.Contacts;
 
 public class MainActivity extends BridgeActivity {
 
@@ -16,7 +16,8 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-      add(ch.byrds.capacitor.contacts.Contacts.class);
+      //add(ch.byrds.capacitor.contacts.Contacts.class);
+      add(Contacts.class);
     }});
   }
 }
