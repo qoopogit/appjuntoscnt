39066682b0559dd0de22e8c905a214464edebd16
Alberto Garcia <alberto.garcia@qoopo.net>
2023-02-27 10:40:51 -0500

    cambio paquete

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ec.com.cnt.juntas',
+  appId: 'io.juntas.starter',
   appName: 'juntas',
   webDir: 'www',
   bundledWebRuntime: false,
